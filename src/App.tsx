0d0fc29b66f0556c09ba1cd0a1d238d6ba2f72ec
e.vasileva <e.vasileva@3h.ru>
2025-05-19 09:16:16 +0300

    feat: make project

---
+++ src/App.tsx
@@ -0,0 +1,31 @@
+import React from 'react';
+
+const App: React.FC = () => {
+  return (
+    <>
+      <h1>Демо пользовательских хуков</h1>
+      <ol>
+        <li>
+          <h2>Fetch</h2>
+        </li>
+        <li>
+          <h2>LocalStorage</h2>
+        </li>
+        <li>
+          <h2>Hover</h2>
+        </li>
+        <li>
+          <h2>Viewport</h2>
+        </li>
+        <li>
+          <h2>Scroll</h2>
+        </li>
+        <li>
+          <h2>Toggle</h2>
+        </li>
+      </ol>
+    </>
+  );
+};
+
+export default App; 
